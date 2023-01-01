d299ece13f16127f23e1b30e1582cd2ee0f32a39
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-02 01:09:50 +0900

    chore: ドキュメントの内容を正しい内容に更新

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,9 @@ async function bootstrap() {
 
   // Swagger
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('PMS-API')
+    .setDescription('PMSのAPIドキュメント')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
