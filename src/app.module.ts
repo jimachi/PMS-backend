55862d03b5b8b9669cbf3bc508f09b2b669c5824
Yuichiro Iijima <jimaichiro@gmail.com>
2023-01-01 18:54:04 +0900

    add: prismaを使用できるよう設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
